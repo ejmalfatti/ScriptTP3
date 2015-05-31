14e0cc1c96d43c4f0db59ce941b6d4135d12691e
Emanuel <you@example.com>
2015-05-31 16:36:32 -0300

    Tabla. Primer commit

---
+++ tabla.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FECHA_ACTUAL=$(date +%F)
+SEMANAL=$(date +%F --date='+7 day')
+
+
+
+echo "-----------------------------------------------"
+echo "| Archivos modificados | Tamaño de filesystem |"
+echo "-----------------------------------------------"
+echo "| 495                    | 1508 MB            |"
+echo "-----------------------------------------------"
