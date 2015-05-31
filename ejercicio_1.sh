5ad13ba6575e6cd15e8eaf1a7432afbacc6c239a
Emanuel <you@example.com>
2015-05-31 16:39:30 -0300

    Mejorados

--- ejercicio_1.sh
+++ ejercicio_1.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-
+VACIO=" "
 DEV=$(mount | grep " on / " | cut -d' ' -f1)
+echo "===== Debe ingresar una fecha del tipo YYYY-MM-DD ====="
 
-read -p "Ingrese la primer fecha: " FECHA1
-read -p "Ingrese la segunda fecha: " FECHA2
+read -p "Ingrese la primer fecha mayor: " FECHA1
+read -p "Ingrese la segunda fecha menor: " FECHA2
+echo
 
 NUM=$(grep -E "($FECHA1|$FECHA2)" /var/log/filesystem/FREE.log | awk {'print$4'})
 
 NI=$(echo $NUM | awk {'print$1'})
 NF=$(echo $NUM | awk {'print$2'})
 
-if [ `expr $NF - $NI` -ge 0 ]; then
-	echo "El filesystem raiz ($DEV) ha crecido: `expr $NF - $NI`"
-elif [ `expr $NF - $NI` -le 0 ]; then
-	echo "El filesystem raiz ($DEV) ha decrecido: `expr $NI - $NF`"
+if [ $NI = $VACIO 2>/dev/null ] || [ $NF = $VACIO 2>/dev/null ];then
+    echo "El filesystem raiz ($DEV) ha crecido: Error en las fechas o informacion incompleta"
+else
+    if [ `expr $NF - $NI` -ge 0 ]; then
+	    echo "El filesystem raiz ($DEV) ha crecido: `expr $NF - $NI` MB"
+    elif [ `expr $NF - $NI` -le 0 ]; then
+	    echo "El filesystem raiz ($DEV) ha decrecido: `expr $NF - $NI` MB"
+    fi
 fi
+echo
+echo "Archivos modificados desde la fecha $FECHA1 hasta la fecha $FECHA2"
+echo
+grep -e "$FECHA1" -e "$FECHA2" /var/log/filesystem/ARCHIVES.log | sort -u
+
--- registro_log.sh
+++ registro_log.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 IFS=""
+
+check_dir(){
+    DIR="/var/log/filesystem"
+    if [ -d $DIR ]; then
+        echo "Fichero existe"
+    else
+        mkdir -p $DIR
+    fi
+}
+
 FECHA=$(date "+%F")
 DEV=$(mount | grep " on / " | cut -d' ' -f1)
 
 FREE=$(df -mP $DEV | grep -iv "disponibles" | awk {'print$4'}) 
+
+check_dir
+
+find /etc -type f -mtime -1 -exec ls -gGh --full-time '{}' \; | awk {'print$4,$5,$7'} >> /var/log/filesystem/ARCHIVES.log
 
 printf "%s\n" "$FECHA   Espacio libre: $FREE MB" >> /var/log/filesystem/FREE.log
